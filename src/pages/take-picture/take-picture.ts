b015bc5115800a403ce2ca69620a72eb1e6d3bc4
Carlos A Vaz de Farias <carlosaugustovazrodrigues@gmail.com>
2018-10-22 13:56:22 -0300

    protottipo tela de fotos

--- src/pages/take-picture/take-picture.ts
+++ src/pages/take-picture/take-picture.ts
@@ -1,12 +1,16 @@
-import {Component} from '@angular/core';
-import {NavController} from 'ionic-angular';
+import { Component } from '@angular/core';
+import { ViewController } from 'ionic-angular';
 
 @Component({
-    selector:'Page-take-picture',
-    templateUrl:'take-picture.html'
+    selector: 'Page-take-picture',
+    templateUrl: 'take-picture.html'
 })
 
-export class TakePicturePage{
-    constructor(public navCtrl: NavController){
+export class TakePicturePage {
+    constructor(
+        private viewCtrl: ViewController) {
+    }
+    dismiss() {
+        this.viewCtrl.dismiss();
     }
 }
